aaea202000a6d5619c84e0bb2844393d6f70c091
Alberto Arias <nicolas.arias@bancofinandina.com>
2023-06-07 12:08:35 -0500

    contact update

--- src/app/router.ts
+++ src/app/router.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
         path: "", loadComponent: () => import('./templates/principal/principal.component').then(c => c.PrincipalComponent), children: [
             { path: "", loadComponent: () => import('./views/main/main.component').then(c => c.MainComponent), },
             { path: "about", loadComponent: () => import('./views/about/about.component').then(c => c.AboutComponent), },
+            { path: "contact", loadComponent: () => import('./views/contact/contact.component').then(c => c.ContactComponent), },
         ]
     },
     { path: "**", loadComponent: () => import("./templates/error/error.component").then(c => c.ErrorComponent) }
---
+++ src/app/views/contact/contact.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule,FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, AngularSvgIconModule],
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent {
+    private readonly Builder = inject(FormBuilder)
+    form = this.Builder.group({
+        name: new FormControl(null, [Validators.required,]),
+        phone: new FormControl(null, [Validators.required,]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        message: new FormControl(null, [Validators.required,]),
+    })
+
+    sendForm(form: FormGroup){
+        if (form.invalid) return
+
+    }
+}
