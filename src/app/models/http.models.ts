6a6f764c4bde187b44208a28223d1d193cec8d03
Alberto Arias <nicolas.arias@bancofinandina.com>
2023-06-11 18:43:55 -0500

    adding badges

---
+++ src/app/models/http.models.ts
@@ -0,0 +1,4 @@
+export interface BadgeModel {
+    src: string
+    alt: string
+}
--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { BadgeModel } from 'src/app/models/http.models';
 
 @Component({
   selector: 'app-about',
@@ -8,7 +10,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+
+    private Http = inject(HttpClient)
+    public Badges:BadgeModel[] = []
+
+    ngOnInit(): void {
+        this.Http.get<BadgeModel[]>('/assets/documents/badges.json').subscribe(res => this.Badges = res)
+    }
+
     sendTo(URL: string) {
         window.open(URL);
     }
