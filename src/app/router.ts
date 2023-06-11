84247c5b7df7549472706f02ae5d495b32c32543
Alberto Arias <nicolas.arias@bancofinandina.com>
2023-06-11 17:13:48 -0500

    about update

--- src/app/router.ts
+++ src/app/router.ts
@@ -3,9 +3,9 @@ import { Routes } from '@angular/router'
 const routes: Routes = [
     {
         path: "", loadComponent: () => import('./templates/principal/principal.component').then(c => c.PrincipalComponent), children: [
-            { path: "", loadComponent: () => import('./views/main/main.component').then(c => c.MainComponent), },
-            { path: "about", loadComponent: () => import('./views/about/about.component').then(c => c.AboutComponent), },
-            { path: "contact", loadComponent: () => import('./views/contact/contact.component').then(c => c.ContactComponent), },
+            { path: "", loadComponent: () => import('./views/main/main.component').then(c => c.MainComponent), title:'Alberto Arias | Blog'},
+            { path: "about", loadComponent: () => import('./views/about/about.component').then(c => c.AboutComponent), title: 'Alberto Arias | Sobre mi'},
+            { path: "contact", loadComponent: () => import('./views/contact/contact.component').then(c => c.ContactComponent), title: 'Alberto Arias | Contácteme'},
         ]
     },
     { path: "admiñ", loadComponent: () => import("./templates/admin/admin.component").then(c => c.AdminComponent) },
