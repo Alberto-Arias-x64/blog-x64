094ab5839d7c5c1cb022646dd63d8c43f0a58825
Alberto Arias <alberto.arias.x64@gmail.com>
2023-06-15 17:19:36 -0500

    update and consume post

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from 'src/app/components/slider/slider.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
@@ -9,6 +10,13 @@ import { SliderComponent } from 'src/app/components/slider/slider.component';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit{
+    private Http = inject(HttpClient)
+    posts: any
 
+    ngOnInit(): void {
+        this.Http.get('/assets/documents/posts.json').subscribe(res => {
+            this.posts = res
+        })
+    }
 }
