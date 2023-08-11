991e6b7280af42d20c6740f93f8fd5968146894d
Albertox64 <alberto.arias.x64@gmail.com>
2023-08-10 21:28:18 -0500

    update format

--- src/app/interfaces/modal.interface.ts
+++ src/app/interfaces/modal.interface.ts
@@ -1,15 +1,15 @@
 import { ImageInterface } from './http.interface'
 
 export interface ModalInterface {
-    title: string
-    image: ImageInterface
-    description: string
+    title:          string
+    image:          ImageInterface
+    description:    string
     buttonPrincipal: {
-        text: string
-        action: Function | null
+        text:       string
+        action:     Function | null
     },
     buttonSecondary?: {
-        text: string
-        action: Function | null
+        text:       string
+        action:     Function | null
     }
 }
