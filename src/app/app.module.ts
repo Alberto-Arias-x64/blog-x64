63bf2c15a95a97f02c10b459741cfef94db0217f
Alberto Arias <alberto.arias.x64@gmail.com>
2024-08-15 08:49:48 -0500

    (update) main: Rise main version 16 => 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import routes from './router'
 
 import { AngularSvgIconModule } from 'angular-svg-icon'
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { AppComponent } from './app.component'
 import { RouterModule } from '@angular/router'
 import { QuicklinkStrategy } from 'ngx-quicklink'
@@ -12,30 +12,24 @@ import { ModalComponent } from './shared/modal/modal.component'
 import { TokenInterceptor } from './interceptors/token.interceptor';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-@NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
         BrowserAnimationsModule,
         ModalComponent,
-        HttpClientModule,
         AngularSvgIconModule.forRoot(),
         MarkdownModule.forRoot({ loader: HttpClient }),
         RouterModule.forRoot(routes, { preloadingStrategy: QuicklinkStrategy }),
         ServiceWorkerModule.register('ngsw-worker.js', {
-          enabled: !isDevMode(),
-          // Register the ServiceWorker as soon as the application is stable
-          // or after 30 seconds (whichever comes first).
-          registrationStrategy: 'registerWhenStable:30000'
-        })
-    ],
-    providers: [
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })], providers: [
         {
             provide: HTTP_INTERCEPTORS,
             useClass: TokenInterceptor,
             multi: true
-        }
-    ],
-    bootstrap: [AppComponent]
-})
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
